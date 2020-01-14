45a0aeb5146eb596427491ef88e8ee6b8b0a087c
Luis Aponte Lopez <56132978+LuisAponteLopez@users.noreply.github.com>
2020-01-14 19:31:36 -0400

    number invested
    This program invests a number string.

---
+++ number_invested.py
@@ -0,0 +1,9 @@
+x="*"
+print(x*40)
+print("This program invests a number string.")
+print(x*40)
+
+number = input("Enter a number: ")
+number.split()
+print(number,"invested is",number[::-1])
+print("Thanks for using program!")
