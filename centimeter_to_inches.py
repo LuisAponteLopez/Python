96d7f93550d73ec4e21ac71c5f0b05a7732131ed
Luis Aponte Lopez <56132978+LuisAponteLopez@users.noreply.github.com>
2020-01-14 19:21:15 -0400

    centimeter to inches
    This program  converter centimeter to inch

---
+++ centimeter_to_inches.py
@@ -0,0 +1,9 @@
+x="*"
+print(x*40)
+print("This program converter centimeter to inch .")
+print(x*40)
+
+C=eval(input("Enter the measure in centimeter: "))
+P = C*.39370
+print(C,"cm is equal to",round(P,2),"in")
+print("Thanks for using the program")
