03f4389c3309ea46a1275ee5b1ac8ee588743cc8
Luis Aponte Lopez <56132978+LuisAponteLopez@users.noreply.github.com>
2020-01-15 10:32:40 -0400

    library datetime
    This programa determine how old are you.

---
+++ your_age.py
@@ -0,0 +1,19 @@
+from datetime import date
+
+print("*"*100)
+print("This programa determine how old are you.")
+print("*"*100)
+
+
+print("\nEnter your bith date in number. ")
+day = int(input("Day:" ))
+month = int(input("Month: "))
+year = int(input("Year: "))
+if day <=  today.day and month <= today.month:
+    age = today.year - year
+    print("\nYou are",age,"years old.")
+else :
+    age = (today.year-year)-1
+    print("\nYou are",age,"years old.")
+print("\nThanks for using the program, come back soon.")
+    
